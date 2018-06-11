956ce8bb507e308a779fe429af23d1d147e258b8
dohien <dohien11091999@gmail.com>
2018-06-11 18:15:30 +0700

    add search

---
+++ search/TableViewController.swift
@@ -0,0 +1,120 @@
+//
+//  TableViewController.swift
+//  search
+//
+//  Created by dohien on 6/11/18.
+//  Copyright © 2018 hiền hihi. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController , UISearchResultsUpdating{
+    let tableData = ["hihi","huhu","haha","hoho","hyhy"]
+
+    var filteredTableData = [String]()
+    var resultSearchController = UISearchController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.resultSearchController = ({
+            let controller = UISearchController(searchResultsController: nil)
+            controller.searchResultsUpdater = self
+            controller.dimsBackgroundDuringPresentation = false
+            controller.searchBar.sizeToFit()
+//            controller.hidesNavigationBarDuringPresentation = true
+
+            self.tableView.tableHeaderView = controller.searchBar
+            return controller
+        })()
+        self.tableView.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        if (self.resultSearchController.isActive){
+            return filteredTableData.count
+        }
+        else{
+            return tableData.count
+        }
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        if (self.resultSearchController.isActive){
+            cell.textLabel?.text = filteredTableData[indexPath.row]
+            return cell
+        }else{
+            cell.textLabel?.text = tableData[indexPath.row]
+            return cell
+        }
+
+
+    }
+    func updateSearchResults(for searchController: UISearchController) {
+        filteredTableData.removeAll(keepingCapacity: false)
+        let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
+        let array = (tableData as NSArray).filtered(using: searchPredicate)
+        filteredTableData = array as! [String]
+        self.tableView.reloadData()
+    }
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ search/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  search
+//
+//  Created by dohien on 6/11/18.
+//  Copyright © 2018 hiền hihi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
