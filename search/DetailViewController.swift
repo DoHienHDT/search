4f2516b21000686d47006446537288803306fb05
dohien <dohien11091999@gmail.com>
2018-06-12 16:14:59 +0700

    add them sua xoa

--- search/DetailViewController.swift
+++ search/DetailViewController.swift
@@ -25,14 +25,13 @@ class DetailViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        name = nameTextField.text
     }
-    */
+    
 
 }
--- search/TableViewController.swift
+++ search/TableViewController.swift
@@ -9,64 +9,65 @@
 import UIKit
 
 class TableViewController: UITableViewController , UISearchResultsUpdating{
-    let tableData = ["hihi","huhu","haha","hoho","hyhy"]
-
+    var tableData = ["hihi","huhu","haha","hoho","hyhy"]
     var filteredTableData = [String]()
-    var resultSearchController = UISearchController()
- 
+    let controller = UISearchController(searchResultsController : nil)
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.resultSearchController = ({
-            let controller = UISearchController(searchResultsController: nil)
-            controller.searchResultsUpdater = self
-            controller.dimsBackgroundDuringPresentation = false
-            controller.searchBar.sizeToFit()
-//            self.tableView.tableHeaderView = controller.searchBar
-//            controller.hidesNavigationBarDuringPresentation = false
-            tableView.tableHeaderView = controller.searchBar
-            // truyền sang mất nút search
-            definesPresentationContext = true
-            return controller
-        })()
+        controller.searchBar.placeholder = "hihi"
+        controller.searchResultsUpdater = self
+        controller.dimsBackgroundDuringPresentation = false
+        controller.searchBar.sizeToFit()
+                    self.tableView.tableHeaderView = controller.searchBar
+                    controller.hidesNavigationBarDuringPresentation = false
+        tableView.tableHeaderView = controller.searchBar
+        // truyền sang mất nút search
+        definesPresentationContext = true
+        
+        
         self.tableView.reloadData()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        if (self.resultSearchController.isActive){
+        if controller.isActive{
             return filteredTableData.count
         }
-
+            
         else{
             return filteredTableData.count
         }
     }
-
-
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-// có thể tabledata
-        if (self.resultSearchController.isActive){
+        // có thể tabledata
+        if controller.isActive{
             cell.textLabel?.text = filteredTableData[indexPath.row]
             return cell
         }else{
             cell.textLabel?.text = filteredTableData[indexPath.row]
             return cell
         }
-
-
+        
+        
     }
     func updateSearchResults(for searchController: UISearchController) {
         filteredTableData.removeAll(keepingCapacity: false)
@@ -76,50 +77,78 @@ class TableViewController: UITableViewController , UISearchResultsUpdating{
         self.tableView.reloadData()
     }
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        switch editingStyle {
+        case .delete:
+            if controller.textInputMode != nil{
+                if let index = filteredTableData.index(of: filteredTableData[indexPath.row]) {
+                    tableData.remove(at: index)
+                    filteredTableData.remove(at: indexPath.row)
+                    tableView.deleteRows(at: [indexPath], with:.fade )
+                } else{
+                    tableData.remove(at: indexPath.row)
+                    tableView.deleteRows(at: [indexPath], with: .fade)
+                }
+            }
+        default:
+            print("hehe")
         }
+        
+        //        if editingStyle == .delete {
+        //            if(self.resultSearchController.isActive){
+        //                filteredTableData.remove(at: indexPath.row)
+        //            }else{
+        //                tableData.remove(at: indexPath.row)
+        //            }
+        //            tableView.deleteRows(at: [indexPath], with: .fade)
+        //        }
+        //        tableView.reloadData()
     }
-    */
-
+    
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
+        let detailviewcontroller = unwindSegue.source as? DetailViewController
+        if let index = tableView.indexPathForSelectedRow{
+            tableData[index.row] = detailviewcontroller?.name ?? ""
+        }else {
+            tableData.append(detailviewcontroller?.name ?? "")
+        }
+        tableView.reloadData()
     }
-    */
-
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let detailviewcontroller = segue.destination as? DetailViewController
         if let index = tableView.indexPathForSelectedRow{
-           detailviewcontroller?.name = tableData[index.row]
+            detailviewcontroller?.name = tableData[index.row]
         }
     }
     
-
+    
 }
